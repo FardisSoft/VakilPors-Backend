8f392550f5d055b17a30b858c2c68c8e5e0bb19e
M.S.Poulaei <sadeghpoolaee@gmail.com>
2023-03-14 22:09:23 +0330

    add sqlite database configuration

---
+++ src/VakilPors/VakilPors.Data/VakilPorsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VakilPors.Data
+{
+    public class VakilPorsDbContext : DbContext
+    {
+        public VakilPorsDbContext(DbContextOptions<VakilPorsDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
