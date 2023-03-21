07b1e87dc246b13c4c0ffa2d5957bab90a8b3379
Milad <milad.liri@yahoo.com>
2023-03-21 13:24:43 +0330

    generic repo interface added

---
+++ src/VakilPors/VakilPors.Core/Contracts/Repositories/IGenericRepo.cs
@@ -0,0 +1,14 @@
+﻿
+namespace VakilPors.Contracts.Repositories;
+
+public interface IGenericRepo<TEntity> where TEntity : class
+{
+    public IQueryable<TEntity> AsQueryable();
+    public Task<TEntity> FindAsync(object key);
+    public Task<TEntity> FindAsync(object[] keyValues);
+    public Task AddAsync(TEntity entity);
+    public Task AddRangeAsync(IEnumerable<TEntity> entities);
+    public void Update(TEntity entity);
+    public void Remove(TEntity entity);
+    public void RemoveRange(IEnumerable<TEntity> entities);
+}
