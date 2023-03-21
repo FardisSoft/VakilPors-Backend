4b925dd4b2fdcb960e0c62c18b8e647d0c3d5534
Milad <milad.liri@yahoo.com>
2023-03-21 13:23:38 +0330

    user entity added

---
+++ src/VakilPors/VakilPors.Core/Domain/Entities/User.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VakilPors.Core.Domain.Entities;
+
+[Table(nameof(User))]
+public class User 
+{
+    [Key]
+    public int UserId { get; set; }
+
+    [Required]
+    public string Username { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [MaxLength(256)]
+    public string Password { get; set; }
+
+    [Required]
+    [MaxLength(256)]
+    public string PasswordSalt { get; set; }
+
+    [Required]
+    public bool IsActive { get; set; }
+
+    [Required]
+    public bool IsLocked { get; set; }
+}
+
