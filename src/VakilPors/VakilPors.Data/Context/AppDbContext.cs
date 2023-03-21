fe738d487c57f8f48485f657f68e73bde1a24240
Milad <milad.liri@yahoo.com>
2023-03-21 13:54:58 +0330

    app db context added

---
+++ src/VakilPors/VakilPors.Data/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VakilPors.Core.Domain.Entities;
+using VakilPors.Data.Extensions;
+
+namespace VakilPors.Data.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+   
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasIndex(user => user.Username).IsUnique();
+
+        modelBuilder.RegisterEntities(typeof(User).Assembly);
+    }
+}
+
