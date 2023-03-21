522ff62236de86b73a650403650bd961d73bca43
Milad <milad.liri@yahoo.com>
2023-03-21 13:54:00 +0330

    register entities extension method added

---
+++ src/VakilPors/VakilPors.Data/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using VakilPors.Shared.Entities;
+
+namespace VakilPors.Data.Extensions;
+
+public static class ModelBuilderExtensions
+{
+    public static void RegisterEntities(this ModelBuilder modelBuilder, params Assembly[] assemblies)
+    {
+        IEnumerable<Type> types = assemblies.SelectMany(a => a.GetExportedTypes())
+            .Where(c => c.IsClass && !c.IsAbstract && c.IsPublic && typeof(IEntity).IsAssignableFrom(c));
+
+        foreach (Type type in types)
+            modelBuilder.Entity(type);
+    }
+}
--- src/VakilPors/VakilPors.Shared/Entities/IEntity.cs
+++ src/VakilPors/VakilPors.Shared/Entities/IEntity.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace VakilPors.Shared.Entities;
 
-namespace VakilPors.Shared.Entities
+public interface IEntity
 {
-    internal interface IEntity
-    {
-    }
 }
+
