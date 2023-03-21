8df93a3d9039fafdfe3390489221e3b4d2499405
Milad <milad.liri@yahoo.com>
2023-03-21 14:04:32 +0330

    services and dbcontext registered in api

--- src/VakilPors/VakilPors.Web/Program.cs
+++ src/VakilPors/VakilPors.Web/Program.cs
@@ -1,18 +1,23 @@
-using Microsoft.EntityFrameworkCore;
 
+using VakilPors.Business.Extensions;
+using VakilPors.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
 
 // Add services to the container.
+builder.Services.RegisterServices();
+builder.Services.RegisterAppDbContext(configuration);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
