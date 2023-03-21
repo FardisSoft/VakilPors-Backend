c4765204f05800c8f95a930b132040c83689f40e
Milad <milad.liri@yahoo.com>
2023-03-21 13:55:59 +0330

    generic repo implemented

---
+++ src/VakilPors/VakilPors.Data/Repositories/GenericRepo.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VakilPors.Contracts.Repositories;
+using VakilPors.Data.Context;
+
+namespace VakilPors.Data.Repositories;
+
+public class GenericRepo<TEntity> : IGenericRepo<TEntity> where TEntity : class
+{
+    protected int CurrentUserId; 
+
+    protected DbSet<TEntity> Entities;
+    public GenericRepo(AppDbContext appDbContext) 
+        => Entities = appDbContext.Set<TEntity>();
+    
+    public IQueryable<TEntity> AsQueryable()
+        => Entities;
+
+    public async Task<TEntity> FindAsync(object[] keyValues)
+        => await Entities.FindAsync(keyValues);
+
+    public async Task<TEntity> FindAsync(object key)
+        => await Entities.FindAsync(key);
+
+    public async Task AddAsync(TEntity entity)
+         => await Entities.AddAsync(entity);
+    
+    public async Task AddRangeAsync(IEnumerable<TEntity> entities) 
+        => await Entities.AddRangeAsync(entities);
+    
+    public void Update(TEntity entity)
+        => Entities.Update(entity);
+    
+    public void Remove(TEntity entity)
+        => Entities.Remove(entity);
+
+    public void RemoveRange(IEnumerable<TEntity> entities)
+        => Entities.RemoveRange(entities);
+
+}
+
