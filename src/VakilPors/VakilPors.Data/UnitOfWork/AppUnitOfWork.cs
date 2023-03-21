ba58899f88f67e659407adb53953b829f4bdf968
Milad <milad.liri@yahoo.com>
2023-03-21 13:56:34 +0330

    unit of work implemented

---
+++ src/VakilPors/VakilPors.Data/UnitOfWork/AppUnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using VakilPors.Contracts.Repositories;
+using VakilPors.Contracts.UnitOfWork;
+using VakilPors.Core.Domain.Entities;
+using VakilPors.Data.Context;
+
+namespace VakilPors.Data.UnitOfWork;
+
+public class AppUnitOfWork : IAppUnitOfWork
+{
+    private readonly AppDbContext _dbContext;
+
+    public AppUnitOfWork(AppDbContext dbContext)
+        => _dbContext = dbContext;
+
+    private DatabaseFacade _database => _dbContext.Database;
+
+    public async Task<int> SaveChangesAsync()
+        => await _dbContext.SaveChangesAsync();
+
+
+    #region Repos
+
+    public IGenericRepo<User> UserRepo => _dbContext.GetService<IGenericRepo<User>>();
+
+    #endregion
+
+}
