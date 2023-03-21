0472c05cee2ade283ae2a236bf26ac1b7c891852
Milad <milad.liri@yahoo.com>
2023-03-21 13:25:03 +0330

    unit of work interface added

---
+++ src/VakilPors/VakilPors.Core/Contracts/UnitOfWork/IAppUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using VakilPors.Contracts.Repositories;
+using VakilPors.Core.Domain.Entities;
+
+namespace VakilPors.Contracts.UnitOfWork;
+
+public interface IAppUnitOfWork
+{
+    public Task<int> SaveChangesAsync();
+    
+    #region Repos
+
+    public IGenericRepo<User> UserRepo { get; }
+    
+    #endregion
+}
+
