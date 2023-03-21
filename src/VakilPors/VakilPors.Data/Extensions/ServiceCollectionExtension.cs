e3efe611ddc6964bae811b1fe6d05ce2be181a39
Milad <milad.liri@yahoo.com>
2023-03-21 13:57:08 +0330

    register context service collection extension added

---
+++ src/VakilPors/VakilPors.Data/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VakilPors.Contracts.Repositories;
+using VakilPors.Contracts.UnitOfWork;
+using VakilPors.Data.Context;
+using VakilPors.Data.Repositories;
+using VakilPors.Data.UnitOfWork;
+
+namespace VakilPors.Data.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void RegisterAppDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>((IServiceProvider serviceProvider, DbContextOptionsBuilder options) =>
+        {
+            options.UseSqlite(configuration.GetConnectionString("AppDbContext"));
+        });
+        services.AddScoped(typeof(IGenericRepo<>), typeof(GenericRepo<>));
+        services.AddScoped<IAppUnitOfWork, AppUnitOfWork>();
+    }
+}
+
