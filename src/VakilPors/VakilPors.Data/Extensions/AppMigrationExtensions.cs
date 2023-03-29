8db37fa5982dfe9d5a7410996a50b4335b707aa6
M.S.Poulaei <sadeghpoolaee@gmail.com>
2023-03-29 22:14:00 +0330

    add default admin

--- src/VakilPors/VakilPors.Data/Extensions/AppMigrationExtensions.cs
+++ src/VakilPors/VakilPors.Data/Extensions/AppMigrationExtensions.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using VakilPors.Core.Domain.Entities;
 using VakilPors.Data.Context;
 
 namespace VakilPors.Data.Extensions
@@ -18,6 +20,19 @@ namespace VakilPors.Data.Extensions
                 await using var scope = app.Services.CreateAsyncScope();
                 using var db = scope.ServiceProvider.GetService<AppDbContext>();
                 await db.Database.MigrateAsync();
+                bool hasAdmin=await db.Roles.AnyAsync(r=>r.Name==RoleNames.Admin);
+                if (!hasAdmin){
+                    //add default admin
+                    using var userManager = scope.ServiceProvider.GetService<UserManager<User>>();
+                    var admin=new User(){
+                        Email="admin@fardissoft.ir",
+                        Name="Admin",
+                        PhoneNumber="09116863556",
+                        UserName="09116863556"
+                    };
+                    await userManager.CreateAsync(admin,"admin");
+                    await userManager.AddToRoleAsync(admin,RoleNames.Admin);
+                }
             }
             catch (System.Exception)
             {
