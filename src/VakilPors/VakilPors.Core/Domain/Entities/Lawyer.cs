220827093b9d818846e7a0ed57409f9e1c00f7b7
Alireza Eslamikhah <74321517+AlirezaEslamikhah@users.noreply.github.com>
2023-04-11 15:18:23 +0330

    Adding some Features to Lawyer

--- src/VakilPors/VakilPors.Core/Domain/Entities/Lawyer.cs
+++ src/VakilPors/VakilPors.Core/Domain/Entities/Lawyer.cs
@@ -18,5 +18,35 @@ namespace VakilPors.Core.Domain.Entities
         public int UserId { get; set; }
         [ForeignKey(nameof(UserId))]
         public User User { get; set; }
+        public byte[] ProfilePicture { get; set; }
+        public byte[] ProfileBackgroundPicture { get; set; }
+        public bool IsOnline { get; set; }
+        public string Name { get; set; }
+        public string Field {get; set; }
+        public int NumberOfRates { get; set; } = 0;
+        public string city { get; set; }
+        [Range(1, 3)]
+        public int Grade { get; set; }
+        [RegularExpression(@"^\d{5}$")]
+        public long LicenseNumber { get; set; }
+        public List<string> Specialists { get; set; }
+        public int YearsOfExperience { get; set; }
+        [RegularExpression(@"^(Male|Female)$", ErrorMessage = "Gender must be either Male or Female.")]
+        public string Gender { get; set; }
+        public string EducationField { get; set; }
+        public string OfficeAddress { get; set; }
+        public int NumberOfConsultations { get; set; }
+        public int NumberOfAnswers { get; set; }
+        public int NumberOfLikes { get; set; }
+        public int NumberOfVerifies { get; set; }
+        public string AboutMe { get; set; }
+        public byte[] CallingCard { get; set; }
+        public string ResumeLink { get; set; }
+
+        
+
+
+
+
     }
 }
