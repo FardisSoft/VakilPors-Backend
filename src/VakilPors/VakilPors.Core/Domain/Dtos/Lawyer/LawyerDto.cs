f4a53b781d12faa04c53ca73794920ee39bf2d56
Milad <milad.liri@yahoo.com>
2023-04-13 08:13:58 +0430

    lawyer entity and dto updated

--- src/VakilPors/VakilPors.Core/Domain/Dtos/Lawyer/LawyerDto.cs
+++ src/VakilPors/VakilPors.Core/Domain/Dtos/Lawyer/LawyerDto.cs
@@ -1,5 +1,8 @@
+using JetBrains.Annotations;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using VakilPors.Core.Domain.Dtos.User;
@@ -8,9 +11,23 @@ namespace VakilPors.Core.Domain.Dtos.Lawyer
 {
     public record LawyerDto
     {
-        public double Rating { get; set; }
+        public int Id { get; set; }
+        public double Rating { get; set; } = 0d;
         public string ParvandeNo { get; set; }
-        public bool IsAuthorized { get; set; }=false;
+        public bool IsAuthorized { get; set; } = false;
+        public string ProfileImageUrl { get; set; }
+        public string Title { get; set; }
+        public string City { get; set; }
+        public byte Grade { get; set; }
+        public string LicenseNumber { get; set; }
+        public string MemberOf { get; set; }
+        public int YearsOfExperience { get; set; }
+        public string OfficeAddress { get; set; }
+        public string Education { get; set; }
+        public string AboutMe { get; set; }
+        public string CallingCardImageUrl { get; set; }
+        public string ResumeLink { get; set; }
+        public string Specialties { get; set; }
         public UserDto User { get; set; }
     }
 }
--- src/VakilPors/VakilPors.Core/Domain/Entities/Lawyer.cs
+++ src/VakilPors/VakilPors.Core/Domain/Entities/Lawyer.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 using VakilPors.Shared.Entities;
 
 namespace VakilPors.Core.Domain.Entities
@@ -14,7 +16,22 @@ namespace VakilPors.Core.Domain.Entities
         public int Id { get; set; }
         public double Rating { get; set; }=0d;
         public string ParvandeNo { get; set; }
-        public bool IsAuthorized { get; set; }=false;
+        public bool IsAuthorized { get; set; } = false;
+        public string ProfileImageUrl { get; set; }
+        public string Title { get; set; }
+        public string City { get; set; }
+        [ValueRange(1,3)]
+        public byte Grade { get; set; }
+        [StringLength(5)]
+        public string LicenseNumber  { get; set; }
+        public string MemberOf { get; set; }
+        public int YearsOfExperience { get; set; }
+        public string OfficeAddress { get; set; }
+        public string Education { get; set; }
+        public string AboutMe { get; set; }
+        public string CallingCardImageUrl { get; set; }
+        public string ResumeLink { get; set; }
+        public string Specialties { get; set; }
         public int UserId { get; set; }
         [ForeignKey(nameof(UserId))]
         public User User { get; set; }
