af7c32f0d243e1dbd234932c0eb0337706cf9e1e
Milad <milad.liri@yahoo.com>
2023-04-13 10:03:09 +0430

    add forum entities

---
+++ src/VakilPors/VakilPors.Core/Domain/Entities/Thread.cs
@@ -0,0 +1,25 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using VakilPors.Shared.Entities;
+
+namespace VakilPors.Core.Domain.Entities;
+
+public class Thread : IEntity
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public int LikeCount { get; set; }
+
+    public int UserId { get; set; }
+
+    [ForeignKey(nameof(UserId))]
+    public User User { get; set; }
+}
+
---
+++ src/VakilPors/VakilPors.Core/Domain/Entities/ThreadComment.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using VakilPors.Shared.Entities;
+
+namespace VakilPors.Core.Domain.Entities;
+
+public class ThreadComment : IEntity
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Text { get; set; }
+
+    public int LikeCount { get; set; }
+
+    public int UserId { get; set; }
+
+    [ForeignKey(nameof(UserId))]
+    public User User { get; set; }
+
+    public int ThreadId { get; set; }
+
+    [ForeignKey(nameof(ThreadId))]
+    public Thread Thread { get; set; }
+}
+
