6e576317d52a4e8578f64990a301c5e5635fa851
Milad <milad.liri@yahoo.com>
2023-04-26 07:17:42 +0430

    add migration for forum entities

---
+++ src/VakilPors/VakilPors.Data/Migrations/20230426021701_editForumEntities.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VakilPors.Data.Migrations
+{
+    public partial class editForumEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateDate",
+                table: "ThreadComment",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSetAsAnswer",
+                table: "ThreadComment",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateDate",
+                table: "ForumThread",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "HasAnswer",
+                table: "ForumThread",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreateDate",
+                table: "ThreadComment");
+
+            migrationBuilder.DropColumn(
+                name: "IsSetAsAnswer",
+                table: "ThreadComment");
+
+            migrationBuilder.DropColumn(
+                name: "CreateDate",
+                table: "ForumThread");
+
+            migrationBuilder.DropColumn(
+                name: "HasAnswer",
+                table: "ForumThread");
+        }
+    }
+}
