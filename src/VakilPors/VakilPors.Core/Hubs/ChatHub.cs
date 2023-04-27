ccce715303ca62ae4159f322a7b994248158aa55
M.S.Poulaei <sadeghpoolaee@gmail.com>
2023-04-27 13:35:11 +0330

    add logging to chat hub

--- src/VakilPors/VakilPors.Core/Hubs/ChatHub.cs
+++ src/VakilPors/VakilPors.Core/Hubs/ChatHub.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using VakilPors.Contracts.UnitOfWork;
 using VakilPors.Core.Domain.Entities;
 using VakilPors.Core.Exceptions;
@@ -14,24 +15,28 @@ namespace VakilPors.Core.Hubs
     public class ChatHub : Hub<IChatClient>
     {
         private readonly IAppUnitOfWork appUnitOfWork;
+        private readonly ILogger<ChatHub> logger;
 
-        public ChatHub(IAppUnitOfWork appUnitOfWork)
+        public ChatHub(IAppUnitOfWork appUnitOfWork, ILogger<ChatHub> logger)
         {
             this.appUnitOfWork = appUnitOfWork;
+            this.logger = logger;
         }
         public async Task SendMessage(ChatMessage message)
         {
             await appUnitOfWork.ChatMessageRepo.AddAsync(message);
             await appUnitOfWork.SaveChangesAsync();
             message = await appUnitOfWork.ChatMessageRepo.AsQueryableNoTracking().Include(m => m.Sender).FirstOrDefaultAsync(m => m.Id == message.Id);
+            logger.LogInformation($"user {message.Sender.Name} send message {message.Message} to chat id:{message.ChatId}");
             message.Sender.Messages = null;
             await Clients.Group(message.ChatId.ToString()).ReceiveMessage(message);
         }
         public async Task ReadChatMessages(string chatId)
         {
+            var userId = getUserId();
+            logger.LogInformation($"user with id:{userId} read messages of chat id:{chatId}");
             await Clients.Group(chatId).ReadMessages(chatId);
             var chatId_int = Convert.ToInt32(chatId);
-            var userId = getUserId();
             var messages = await appUnitOfWork.ChatMessageRepo.AsQueryable().Where(m => m.ChatId == chatId_int).ToArrayAsync();
             for (int i = 0; i < messages.Length; i++)
             {
@@ -41,10 +46,11 @@ namespace VakilPors.Core.Hubs
         }
         public async Task DeleteChatMessage(string chatId, string messageId)
         {
+            var userId = getUserId();
             var chatId_int = Convert.ToInt32(chatId);
             var messageId_int = Convert.ToInt32(messageId);
             var message = await appUnitOfWork.ChatMessageRepo.FindAsync(messageId_int);
-            var userId = getUserId();
+            logger.LogInformation($"user with id:{userId} delete message {message.Message} of chat id:{chatId}");
             if (message.SenderId != userId)
                 throw new AccessViolationException("You do not have permission to perform this action");
             message.IsDeleted = true;
@@ -57,6 +63,7 @@ namespace VakilPors.Core.Hubs
             var userId = getUserId();
             if (message.SenderId != userId)
                 throw new AccessViolationException("You do not have permission to perform this action");
+            logger.LogInformation($"user with id:{userId} edited message {message.Message} of chat id:{chatId}");
             message.IsEdited = true;
             await Clients.Group(chatId).EditMessage(message);
             appUnitOfWork.ChatMessageRepo.Update(message);
@@ -65,6 +72,7 @@ namespace VakilPors.Core.Hubs
 
         public async Task AddToChat(string chatId)
         {
+            logger.LogInformation($"user with id:{getUserId()} edited joined chat with id:{chatId} with connection id:{Context.ConnectionId}");
             await Groups.AddToGroupAsync(Context.ConnectionId, chatId);
             // var message = new ChatMessage()
             // {
