5db7100aabb8d3fb26f80462d0ebb6948e00e67b
Milad <milad.liri@yahoo.com>
2023-04-28 16:12:55 +0430

    config like entities in context

--- src/VakilPors/VakilPors.Data/Context/AppDbContext.cs
+++ src/VakilPors/VakilPors.Data/Context/AppDbContext.cs
@@ -17,7 +17,7 @@ public class AppDbContext : IdentityDbContext<User, Role, int>
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        base.OnModelCreating(modelBuilder);
+        
 
         modelBuilder.Entity<User>().HasIndex(user => user.UserName).IsUnique();
         modelBuilder.Entity<User>()
@@ -25,6 +25,27 @@ public class AppDbContext : IdentityDbContext<User, Role, int>
             .WithOne(l => l.User)
             .HasForeignKey<Lawyer>(l => l.UserId);
 
+        modelBuilder.Entity<UserCommentLike>()
+            .HasOne<User>(x => x.User)
+            .WithMany(u => u.CommentLikes)
+            .HasForeignKey(x => x.UserId);
+
+        modelBuilder.Entity<UserCommentLike>()
+            .HasOne<ThreadComment>(x => x.Comment)
+            .WithMany(c => c.UserLikes)
+            .HasForeignKey(x => x.CommentId);
+
+        modelBuilder.Entity<UserThreadLike>()
+            .HasOne<User>(x => x.User)
+            .WithMany(u => u.ThreadLikes)
+            .HasForeignKey(x => x.UserId);
+
+        modelBuilder.Entity<UserThreadLike>()
+            .HasOne<ForumThread>(x => x.Thread)
+            .WithMany(t => t.UserLikes)
+            .HasForeignKey(x => x.ThreadId);
+
+        base.OnModelCreating(modelBuilder);
 
         modelBuilder.RegisterEntities(typeof(User).Assembly);
         modelBuilder.ApplyUtcDateTimeConverter();
