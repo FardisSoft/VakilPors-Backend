b53b1e282cd8e42b1232341b187b6dd8db0ab94f
Milad <milad.liri@yahoo.com>
2023-04-28 16:12:01 +0430

    add forum like entities migrations

---
+++ src/VakilPors/VakilPors.Data/Migrations/20230428103833_addUserLikeEntities.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace VakilPors.Data.Migrations
+{
+    public partial class addUserLikeEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserCommentLike",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    CommentId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserCommentLike", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserCommentLike_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserCommentLike_ThreadComment_CommentId",
+                        column: x => x.CommentId,
+                        principalTable: "ThreadComment",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserThreadLike",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    ThreadId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserThreadLike", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserThreadLike_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserThreadLike_ForumThread_ThreadId",
+                        column: x => x.ThreadId,
+                        principalTable: "ForumThread",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserCommentLike_CommentId",
+                table: "UserCommentLike",
+                column: "CommentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserCommentLike_UserId",
+                table: "UserCommentLike",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserThreadLike_ThreadId",
+                table: "UserThreadLike",
+                column: "ThreadId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserThreadLike_UserId",
+                table: "UserThreadLike",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserCommentLike");
+
+            migrationBuilder.DropTable(
+                name: "UserThreadLike");
+        }
+    }
+}
