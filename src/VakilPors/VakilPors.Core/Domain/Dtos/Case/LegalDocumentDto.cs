c311b9dfc22e581c154ef76066b6ba67d7098212
Milad <milad.liri@yahoo.com>
2023-04-30 05:14:54 +0430

    add case dtos

---
+++ src/VakilPors/VakilPors.Core/Domain/Dtos/Case/LegalDocumentDto.cs
@@ -0,0 +1,22 @@
+﻿
+
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VakilPors.Core.Domain.Dtos.Case
+{
+    public class LegalDocumentDto
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public string FileUrl { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public IFormFile File { get; set; }
+    }
+}
