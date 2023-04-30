da17ec0e4704f73b0c8d3cbfacde0efd894368d5
Milad <milad.liri@yahoo.com>
2023-04-30 05:16:10 +0430

    add case migrations

---
+++ src/VakilPors/VakilPors.Data/Migrations/20230430002950_addDocumentEntities.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace VakilPors.Data.Migrations
+{
+    public partial class addDocumentEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LegalDocument",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    FileUrl = table.Column<string>(type: "text", nullable: true),
+                    Title = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LegalDocument", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LegalDocument_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DocumentAccess",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DocumentId = table.Column<int>(type: "integer", nullable: false),
+                    LawyerId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DocumentAccess", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DocumentAccess_Lawyer_LawyerId",
+                        column: x => x.LawyerId,
+                        principalTable: "Lawyer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DocumentAccess_LegalDocument_DocumentId",
+                        column: x => x.DocumentId,
+                        principalTable: "LegalDocument",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DocumentAccess_DocumentId",
+                table: "DocumentAccess",
+                column: "DocumentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DocumentAccess_LawyerId",
+                table: "DocumentAccess",
+                column: "LawyerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LegalDocument_UserId",
+                table: "LegalDocument",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DocumentAccess");
+
+            migrationBuilder.DropTable(
+                name: "LegalDocument");
+        }
+    }
+}
