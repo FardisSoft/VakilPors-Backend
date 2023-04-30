3c06c4348f19f65e379144f0957669a6ec3f420e
Milad <milad.liri@yahoo.com>
2023-04-30 05:16:39 +0430

    add document apis

---
+++ src/VakilPors/VakilPors.Web/Controllers/DocumentController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using VakilPors.Core.Contracts.Services;
+using VakilPors.Core.Domain.Dtos.Case;
+using VakilPors.Core.Domain.Dtos.Lawyer;
+using VakilPors.Core.Domain.Dtos.User;
+using VakilPors.Shared.Response;
+using VakilPors.Web.Controllers;
+
+namespace VakilPors.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class DocumentController : MyControllerBase
+    {
+        private readonly ILegalDocumentService _documentService;
+
+        public DocumentController(ILegalDocumentService documentService)
+        {
+            _documentService = documentService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddDocument([FromForm] LegalDocumentDto documentDto)
+        {
+            var result = await _documentService.AddDocument(getUserId(), documentDto);
+            return Ok(new AppResponse<object>(result, "success"));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateDocument([FromForm] LegalDocumentDto documentDto)
+        {
+            var result = await _documentService.UpdateDocument(documentDto);
+            return Ok(new AppResponse<object>(result, "success"));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteDocument(int documentId)
+        {
+            var result = await _documentService.DeleteDocument(documentId);
+            return Ok(new AppResponse<object>(result, "success"));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDocumentsByUserId(int userId)
+        {
+            var result = await _documentService.GetDocumentsByUserId(userId);
+            return Ok(new AppResponse<object>(result, "success"));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDocumentById(int documentId)
+        {
+            var result = await _documentService.GetDocumentById(documentId);
+            return Ok(new AppResponse<object>(result, "success"));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GrantAccessToLawyer(DocumentAccessDto documentAccessDto)
+        {
+            var result = await _documentService.GrantAccessToLawyer(documentAccessDto);
+            return Ok(new AppResponse<object>(result, "success"));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> TakeAccessFromLawyer(DocumentAccessDto documentAccessDto)
+        {
+            var result = await _documentService.TakeAccessFromLawyer(documentAccessDto);
+            return Ok(new AppResponse<object>(result, "success"));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetLawyersThatHaveAccessToDocument(int documentId)
+        {
+            var result = await _documentService.GetLawyersThatHaveAccessToDocument(documentId);
+            return Ok(new AppResponse<object>(result, "success"));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsersThatLawyerHasAccessToThairDocuments(int lawyerId)
+        {
+            var result = await _documentService.GetUsersThatLawyerHasAccessToThairDocuments(lawyerId);
+            return Ok(new AppResponse<object>(result, "success"));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetDocumentsThatLawyerHasAccessToByUserId(LawyerDocumentAccessDto lawyerDocumentAccessDto)
+        {
+            var result = await _documentService.GetDocumentsThatLawyerHasAccessToByUserId(lawyerDocumentAccessDto);
+            return Ok(new AppResponse<object>(result, "success"));
+        }
+
+
+    }
+}
