c57952c5fffb4cb7d6723805f74156e957bcf66c
M.S.Poulaei <sadeghpoolaee@gmail.com>
2023-05-19 22:27:59 +0330

    add disconnect user functionality

--- src/VakilPors/VakilPors.Core/Hubs/Clients/IMeetingClient.cs
+++ src/VakilPors/VakilPors.Core/Hubs/Clients/IMeetingClient.cs
@@ -3,4 +3,5 @@ namespace VakilPors.Core.Hubs.Clients;
 public interface IMeetingClient
 {
     Task UserConnected(string userId);
+    Task UserDisconnected(string userId);
 }
--- src/VakilPors/VakilPors.Core/Hubs/MeetingHub.cs
+++ src/VakilPors/VakilPors.Core/Hubs/MeetingHub.cs
@@ -9,6 +9,7 @@ public class MeetingHub : Hub<IMeetingClient>
 {
     private readonly IAppUnitOfWork appUnitOfWork;
     private readonly ILogger<MeetingHub> logger;
+    private static Dictionary<string, string> users = new();
 
     public MeetingHub(IAppUnitOfWork appUnitOfWork, ILogger<MeetingHub> logger)
     {
@@ -19,6 +20,12 @@ public class MeetingHub : Hub<IMeetingClient>
     {
         await Groups.AddToGroupAsync(Context.ConnectionId, meetingId);
         await Clients.Group(meetingId).UserConnected(userId);
+        users.Add(Context.ConnectionId, userId);
+    }
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        Clients.All.UserDisconnected(users[Context.ConnectionId]);
+        return base.OnDisconnectedAsync(exception);
     }
 
 }
