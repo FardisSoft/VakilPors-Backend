1b8d075e43943d1474ad5fb5b3b68d1c5624efee
Alireza Eslamikhah <74321517+AlirezaEslamikhah@users.noreply.github.com>
2023-05-31 23:21:53 +0330

    ADD Telegram to user

---
+++ src/VakilPors/VakilPors.Data/Migrations/20230531194952_telegram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VakilPors.Data.Migrations
+{
+    public partial class telegram : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Telegram",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Telegram",
+                table: "AspNetUsers");
+        }
+    }
+}
