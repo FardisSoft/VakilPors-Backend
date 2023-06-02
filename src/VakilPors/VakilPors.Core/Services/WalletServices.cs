9b491aa3e24ac801b1615f4b3eeb09a5948325d4
Alireza Eslamikhah <74321517+AlirezaEslamikhah@users.noreply.github.com>
2023-06-03 01:10:23 +0330

    comment telegram

--- src/VakilPors/VakilPors.Core/Services/WalletServices.cs
+++ src/VakilPors/VakilPors.Core/Services/WalletServices.cs
@@ -68,10 +68,10 @@ namespace VakilPors.Core.Services
             string body = $@"تراکنش شما در تاریخ {DateTime.Now} ثبت شد.مبلغ: {amount} توضیحات: {description} کد تراکنش: {authority}  تراکنش موفقیت آمیز بود: {isSuccess}";
             var user = await getUser(userId);
             await emailSender.SendEmailAsync(user.Email, user.Name, "تراکنش", body);
-            if (user.Telegram != null)
-            {
-                await TelegramService.SendToTelegram(body, user.Telegram);
-            }
+            //if (user.Telegram != null)
+            //{
+            //    await TelegramService.SendToTelegram(body, user.Telegram);
+            //}
 
 
         }
