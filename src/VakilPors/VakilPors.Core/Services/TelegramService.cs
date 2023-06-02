d945466232fafc43a05ce6ffaa8ba66cefa9f913
Alireza Eslamikhah <74321517+AlirezaEslamikhah@users.noreply.github.com>
2023-06-02 19:14:23 +0330

    Adding Telegram Sending message

--- src/VakilPors/VakilPors.Core/Services/TelegramService.cs
+++ src/VakilPors/VakilPors.Core/Services/TelegramService.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using VakilPors.Contracts.UnitOfWork;
 using VakilPors.Core.Contracts.Services;
 using VakilPors.Core.Domain.Dtos.User;
+using static System.Net.WebRequestMethods;
 
 namespace VakilPors.Core.Services
 {
@@ -21,11 +22,33 @@ namespace VakilPors.Core.Services
             _appUnitOfWork = appUnitOfWork;
             _mapper = mapper;
         }
+
+        
         public async Task SaveChatId(TelegramDto telegram)
         {
             var user = await _appUnitOfWork.UserRepo.AsQueryable().Where(x => x.PhoneNumber == telegram.phone_number).FirstOrDefaultAsync();
             user.Telegram = telegram.chat_id;
             await _appUnitOfWork.SaveChangesAsync();
+        }
+
+        public static Task SendToTelegram(string text, string chat_id)
+        {
+            string jsonData = "{\"chat_id\":\"" + chat_id + "\",\"text\":\"" + text + "\"}";
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var client = new HttpClient();
+            var url = "https://fardissoft.pythonanywhere.com/post";
+            var response = client.PostAsync(url, content);
+            if (response.Result.IsSuccessStatusCode)
+            {
+                return Task.CompletedTask;
+            }
+            else
+            {
+                return Task.FromException(new Exception("Error in sending message to telegram"));
+            }
+
+            
+
         }
     }
 }
