686d7591b2c13b8675e938bf5cef2c7fd0364fa7
M.S.Poulaei <sadeghpoolaee@gmail.com>
2023-07-08 17:41:05 +0330

    add html template for email sender

--- src/VakilPors/VakilPors.Core/Contracts/Services/IEmailSender.cs
+++ src/VakilPors/VakilPors.Core/Contracts/Services/IEmailSender.cs
@@ -8,6 +8,6 @@ namespace VakilPors.Core.Contracts.Services
 {
     public interface IEmailSender : IScopedDependency
     {
-        Task SendEmailAsync(string email, string name, string subject, string message);
+        Task SendEmailAsync(string email, string name, string subject, string message, bool useHtml = true);
     }
 }
--- src/VakilPors/VakilPors.Core/Services/EmailSender.cs
+++ src/VakilPors/VakilPors.Core/Services/EmailSender.cs
@@ -35,7 +35,7 @@ namespace VakilPors.Core.Services
             this._logger = logger;
         }
 
-        public async Task SendEmailAsync(string email, string name, string subject, string htmlMessage)
+        public async Task SendEmailAsync(string email, string name, string subject, string htmlMessage, bool useHtml = true)
         {
             try
             {
@@ -43,10 +43,9 @@ namespace VakilPors.Core.Services
                 message.From.Add(new MailboxAddress(_fromName, _fromEmail));
                 message.To.Add(new MailboxAddress(name, email));
                 message.Subject = subject;
-
                 message.Body = new TextPart(TextFormat.Html)
                 {
-                    Text = htmlMessage
+                    Text = (useHtml ? html.Replace("{{content}}", htmlMessage).Replace("{{title}}", subject) : htmlMessage)
                 };
 
                 using (var client = new SmtpClient())
@@ -57,12 +56,13 @@ namespace VakilPors.Core.Services
 
                     await client.SendAsync(message);
                     await client.DisconnectAsync(true);
-                }   
+                }
             }
             catch (System.Exception)
             {
                 _logger.LogError($"An Error occured while sending Email {_fromEmail} to {email} with subj:{subject} name:{name}");
             }
         }
+        private string html = "<!doctypehtml><html dir=rtl><meta charset=utf-8><meta content=\"ie=edge\"http-equiv=x-ua-compatible><title>Email Confirmation</title><meta content=\"width=device-width,initial-scale=1\"name=viewport><style>@media screen{@font-face{font-family:'Source Sans Pro';font-style:normal;font-weight:400;src:local('Source Sans Pro Regular'),local('SourceSansPro-Regular'),url(https://fonts.gstatic.com/s/sourcesanspro/v10/ODelI1aHBYDBqgeIAH2zlBM0YzuT7MdOe03otPbuUS0.woff) format('woff')}@font-face{font-family:'Source Sans Pro';font-style:normal;font-weight:700;src:local('Source Sans Pro Bold'),local('SourceSansPro-Bold'),url(https://fonts.gstatic.com/s/sourcesanspro/v10/toadOcfmlt9b38dHJxOBGFkQc6VGVFSmCnC_l7QZG60.woff) format('woff')}}a,body,table,td{-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%}table,td{mso-table-rspace:0;mso-table-lspace:0}img{-ms-interpolation-mode:bicubic}a[x-apple-data-detectors]{font-family:inherit!important;font-size:inherit!important;font-weight:inherit!important;line-height:inherit!important;color:inherit!important;text-decoration:none!important}div[style*=\"margin: 16px 0;\"]{margin:0!important}body{width:100%!important;height:100%!important;padding:0!important;margin:0!important}table{border-collapse:collapse!important}a{color:#1a82e2}img{height:auto;line-height:100%;text-decoration:none;border:0;outline:0}</style><body style=background-color:#e9ecef><div class=preheader style=display:none;max-width:0;max-height:0;overflow:hidden;font-size:1px;line-height:1px;color:#fff;opacity:0>A preheader is the short summary text that follows the subject line when an email is viewed in the inbox.</div><table border=0 cellpadding=0 cellspacing=0 width=100%><tr><td align=center bgcolor=#e9ecef><!--[if (gte mso 9)|(IE)]><table border=0 cellpadding=0 cellspacing=0 width=600 align=center><tr><td align=center valign=top width=600><![endif]--><table border=0 cellpadding=0 cellspacing=0 width=100% style=max-width:600px><tr><td align=center style=\"padding:36px 24px\"valign=top><a href=https://www.blogdesire.com style=display:inline-block target=_blank><img alt=Logo border=0 src=https://fardissoft.ir/logoFS.jpg style=display:block;width:48px;max-width:48px;min-width:48px width=48></a></table><!--[if (gte mso 9)|(IE)]><![endif]--><tr><td align=center bgcolor=#e9ecef><!--[if (gte mso 9)|(IE)]><table border=0 cellpadding=0 cellspacing=0 width=600 align=center><tr><td align=center valign=top width=600><![endif]--><table border=0 cellpadding=0 cellspacing=0 width=100% style=max-width:600px><tr><td align=right bgcolor=#ffffff style=\"padding:36px 24px 0;font-family:'Source Sans Pro',Helvetica,Arial,sans-serif;border-top:3px solid #d4dadf\"><h1 style=margin:0;font-size:32px;font-weight:700;letter-spacing:-1px;line-height:48px>{{title}}</h1></table><!--[if (gte mso 9)|(IE)]><![endif]--><tr><td align=center bgcolor=#e9ecef><!--[if (gte mso 9)|(IE)]><table border=0 cellpadding=0 cellspacing=0 width=600 align=center><tr><td align=center valign=top width=600><![endif]--><table border=0 cellpadding=0 cellspacing=0 width=100% style=max-width:600px><tr><td align=right bgcolor=#ffffff style=\"padding:24px;font-family:'Source Sans Pro',Helvetica,Arial,sans-serif;font-size:16px;line-height:24px\"><p style=margin:0>{{content}}</table><!--[if (gte mso 9)|(IE)]><![endif]--><tr><td align=center bgcolor=#e9ecef style=padding:24px><!--[if (gte mso 9)|(IE)]><table border=0 cellpadding=0 cellspacing=0 width=600 align=center><tr><td align=center valign=top width=600><![endif]--><table border=0 cellpadding=0 cellspacing=0 width=100% style=max-width:600px></table><!--[if (gte mso 9)|(IE)]><![endif]--></table>";
     }
 }
