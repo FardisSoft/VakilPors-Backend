6cc6b69f8af8d537157bd6daffe605bb43041e5c
Alireza Eslamikhah <74321517+AlirezaEslamikhah@users.noreply.github.com>
2023-10-20 22:56:26 +0330

    Create SearchDto.cs

---
+++ src/VakilPors/VakilPors.Core/Domain/Dtos/Search/SearchDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VakilPors.Core.Domain.Dtos.Search
+{
+    public record SearchDto
+    {
+        public double Rating { get; set; } = 0d;
+        public string Title { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string MemberOf { get; set; } = string.Empty;
+        public byte Grade { get; set; } = 0;
+        public string LicenseNumber { get; set; } = string.Empty;
+        public string Gender { get; set; } = string.Empty;
+
+
+
+
+
+    }
+}
