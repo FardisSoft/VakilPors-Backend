40c6037763721be42f317d3804f14dbf45f2f209
Alireza Eslamikhah <74321517+AlirezaEslamikhah@users.noreply.github.com>
2023-11-02 23:39:51 +0330

    deactive premium test

--- src/VakilPors/VakilPors.Core/Services/PremiumService.cs
+++ src/VakilPors/VakilPors.Core/Services/PremiumService.cs
@@ -73,10 +73,7 @@ namespace VakilPors.Core.Services
 
         public async Task DeactivatePremium(int user_id)
         {
-            //var subscribed = await _appUnitOfWork.SubscribedRepo.FindAsync(user_id);
-            //subscribed.ExpireDate = DateTime.Now;
-            //_appUnitOfWork.SubscribedRepo.Update(subscribed);
-            //await _appUnitOfWork.SaveChangesAsync();    
+             
             var sub = await _appUnitOfWork.SubscribedRepo.AsQueryable().Where(x => x.UserId == user_id).FirstAsync();
             if (sub == null)
                 throw new BadArgumentException("Subscription Not Found");
