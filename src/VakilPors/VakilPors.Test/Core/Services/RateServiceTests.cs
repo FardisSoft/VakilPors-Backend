8c24e29c6ad0e5f1713df2cbebcc47daf6d89172
Alireza Eslamikhah <74321517+AlirezaEslamikhah@users.noreply.github.com>
2023-11-17 00:28:00 +0330

    Create RateServiceTests.cs

---
+++ src/VakilPors/VakilPors.Test/Core/Services/RateServiceTests.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using MockQueryable.Moq;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VakilPors.Contracts.UnitOfWork;
+using VakilPors.Core.Contracts.Services;
+using VakilPors.Core.Domain.Dtos.Rate;
+using VakilPors.Core.Domain.Entities;
+using VakilPors.Core.Services;
+
+namespace VakilPors.Test.Core.Services
+{
+    public class RateServiceTests
+    {
+        private readonly RateService rateService;
+        private readonly Mock<IAppUnitOfWork> appUnitOfWorkMock;
+        private readonly Mock<IMapper> mapperMock;
+        private readonly Mock<ILawyerServices> lawyerServicesMock;
+
+        public RateServiceTests()
+        {
+            appUnitOfWorkMock = new Mock<IAppUnitOfWork>(); 
+            mapperMock = new Mock<IMapper>();
+            lawyerServicesMock = new Mock<ILawyerServices>();
+
+            rateService = new RateService( mapperMock.Object ,appUnitOfWorkMock.Object, lawyerServicesMock.Object);
+            
+        }
+
+        [Fact]
+        public async Task add_rate_async()
+        {
+            //Arrange 
+            var userid = 1;
+            var lawyerid = 1;
+            var ratedto = new RateDto { RateNum = 3 };
+            var rate = new Rate();
+            var lawyer = new Lawyer { Rating = 0 };
+            IEnumerable<Rate> rates = new List<Rate>
+            {
+
+            };
+            var mock = rates.AsQueryable().BuildMock();
+
+            IEnumerable<Lawyer> lawyers = new List<Lawyer>
+            {
+                new Lawyer { Id = lawyerid , Rating = 0}
+            };
+            var mock2 = lawyers.AsQueryable().BuildMock();
+
+            appUnitOfWorkMock.Setup(x => x.RateRepo.AsQueryable()).Returns(mock);
+            mapperMock.Setup(x => x.Map<Rate>(It.IsAny<RateDto>())).Returns(rate);
+            appUnitOfWorkMock.Setup(x => x.RateRepo.AddAsync(rate)).Returns(Task.CompletedTask);
+            appUnitOfWorkMock.Setup(x => x.LawyerRepo.AsQueryable()).Returns(mock2);
+            lawyerServicesMock.Setup(x=> x.AddToken(It.IsAny<int>(), It.IsAny<int>())).Returns(Task.CompletedTask);
+            appUnitOfWorkMock.Setup(x => x.SaveChangesAsync()).ReturnsAsync(1);
+
+            //Act 
+            await rateService.AddRateAsync(ratedto, userid , lawyerid);
+
+            //Assert 
+            Assert.Equal(3, lawyers.ToList()[0].Rating);
+
+
+        }
+    }
+}
