0eff7d7d90fdaff5c5370ce8c6704813fa87b089
Alireza Eslamikhah <74321517+AlirezaEslamikhah@users.noreply.github.com>
2023-12-16 14:30:08 +0330

    Update ThreadCommentServiceTests.cs

--- src/VakilPors/VakilPors.Test/Core/Services/ThreadCommentServiceTests.cs
+++ src/VakilPors/VakilPors.Test/Core/Services/ThreadCommentServiceTests.cs
@@ -186,6 +186,39 @@ namespace VakilPors.Test.Core.Services
             Assert.Equal(0, result);
         }
 
+        [Fact]
+        public async Task setasanswer()
+        {
+            //Arrange
+            var userid = 1;
+            var commentid = 1;
+            var threadcommentdto = new ThreadCommentDto();
+            IEnumerable<ThreadComment> threadComments = new List<ThreadComment>()
+            {
+                new ThreadComment {UserLikes = new List<UserCommentLike>(){ new UserCommentLike() { UserId = 1 , CommentId = 1} } , Id = 1 , Thread = new ForumThread(){UserId = 1 }  , UserId = 10}
 
+            };
+            var mock = threadComments.BuildMock();
+
+            IEnumerable<Lawyer> lawyers = new List<Lawyer>()
+            {
+                new Lawyer(){UserId =10 }
+            };
+            var mock2 = lawyers.BuildMock();
+
+
+            appUnitOfWorkmock.Setup(u => u.ThreadCommentRepo.AsQueryable()).Returns(mock);
+            appUnitOfWorkmock.Setup(u => u.ThreadCommentRepo.Update(It.IsAny<ThreadComment>()));
+            appUnitOfWorkmock.Setup(u => u.SaveChangesAsync()).ReturnsAsync(1);
+            appUnitOfWorkmock.Setup(u => u.LawyerRepo.AsQueryable()).Returns(mock2);
+            lawyerservicesmock.Setup(u => u.AddToken(It.IsAny<int>(), It.IsAny<int>()));
+
+            //Act 
+            var result =await  threadCommentService.SetAsAnswer(userid, commentid);
+
+            //Assert 
+            Assert.True(result);
+
+        }
     }
 }
