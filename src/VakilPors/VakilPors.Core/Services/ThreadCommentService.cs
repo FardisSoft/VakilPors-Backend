d1ed8ff1e78319cb899c00c3c14546ba575cd15f
Alireza Eslamikhah <74321517+AlirezaEslamikhah@users.noreply.github.com>
2023-12-16 10:45:26 +0330

    initialize

--- src/VakilPors/VakilPors.Core/Services/ThreadCommentService.cs
+++ src/VakilPors/VakilPors.Core/Services/ThreadCommentService.cs
@@ -1,6 +1,4 @@
-﻿
-
-using AutoMapper;
+﻿using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using System.Threading;
---
+++ src/VakilPors/VakilPors.Test/Core/Services/ThreadCommentServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VakilPors.Contracts.UnitOfWork;
+using VakilPors.Core.Contracts.Services;
+using VakilPors.Core.Services;
+
+namespace VakilPors.Test.Core.Services
+{
+    public class ThreadCommentServiceTests
+    {
+        private readonly ThreadCommentService threadCommentService;
+        private readonly Mock<IAppUnitOfWork> appUnitOfWorkmock;
+        private readonly Mock<IMapper> mappermock;
+        private readonly Mock<ILawyerServices> lawyerservicesmock;
+        private readonly Mock<IPremiumService> premiumservicemock;
+
+        public ThreadCommentServiceTests()
+        {
+            appUnitOfWorkmock = new Mock<IAppUnitOfWork>();
+            mappermock = new Mock<IMapper>();
+            lawyerservicesmock = new Mock<ILawyerServices>();
+            premiumservicemock = new Mock<IPremiumService>();
+            threadCommentService = new ThreadCommentService
+                (
+                appUnitOfWorkmock.Object, mappermock.Object, lawyerservicesmock.Object, premiumservicemock.Object
+                );
+        }
+
+    }
+}
